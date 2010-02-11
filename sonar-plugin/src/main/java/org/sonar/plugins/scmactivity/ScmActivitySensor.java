5b68950a73c90b8d006e04b34ce45dc4a2db1749
Godin <mandrikov@gmail.com>
2010-02-11 22:14:15 +0000

    SCM-ACTIVITY: fix a typo

--- sonar-plugin/src/main/java/org/sonar/plugins/scmactivity/ScmActivitySensor.java
+++ sonar-plugin/src/main/java/org/sonar/plugins/scmactivity/ScmActivitySensor.java
@@ -52,7 +52,7 @@ public class ScmActivitySensor implements Sensor {
   public static final boolean ENABLED_DEFAULT_VALUE = false;
   public static final String USER_PROPERTY = "sonar.scm-activity.user.secured";
   public static final String PASSWORD_PROPERTY = "sonar.scm-activity.password.secured";
-  public static final String PREFER_PURE_JAVA_PROPERTY = "sonar.scm-sctivity.prefer_pure_java";
+  public static final String PREFER_PURE_JAVA_PROPERTY = "sonar.scm-activity.prefer_pure_java";
   public static final boolean PREFER_PURE_JAVA_DEFAULT_VALUE = true;
 
   public boolean shouldExecuteOnProject(Project project) {
