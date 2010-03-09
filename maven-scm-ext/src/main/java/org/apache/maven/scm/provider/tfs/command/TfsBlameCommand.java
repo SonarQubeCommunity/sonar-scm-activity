78a1cb9f0c6d111b033e9a3134f52920c70c4dd0
Godin <mandrikov@gmail.com>
2010-03-09 21:17:04 +0000

    SCM Activity: Update to Parent 5

---
+++ maven-scm-ext/src/main/java/org/apache/maven/scm/provider/tfs/command/TfsBlameCommand.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2010 Evgeny Mandrikov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.maven.scm.provider.tfs.command;
+
+import org.apache.maven.scm.ScmException;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.command.blame.AbstractBlameCommand;
+import org.apache.maven.scm.command.blame.BlameScmResult;
+import org.apache.maven.scm.provider.ScmProviderRepository;
+
+/**
+ * @author Evgeny Mandrikov
+ */
+public class TfsBlameCommand extends AbstractBlameCommand {
+  /**
+   * {@inheritDoc}
+   */
+  public BlameScmResult executeBlameCommand(ScmProviderRepository repo, ScmFileSet workingDirectory, String filename)
+      throws ScmException {
+    // TODO
+    return null;
+  }
+}
