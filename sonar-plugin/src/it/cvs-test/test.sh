cccaf3e9892e3933e66473b3233879b3dad8c760
simonbrandhof <simon.brandhof@gmail.com>
2011-03-08 22:01:17 +0000

    SCM plugin: remove unused files

--- sonar-plugin/src/it/cvs-test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir target
-cd target
-cvs -z3 -d:pserver:anonymous@javacaltools.cvs.sourceforge.net:/cvsroot/javacaltools co -P javacaltools
-cp ../pom.xml javacaltools
-cd javacaltools
-mvn sonar:sonar
--- sonar-plugin/src/it/git-test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir target
-cd target
-git clone http://github.com/talios/clojure-maven-plugin.git
-cd clojure-maven-plugin
-mvn clean install
-mvn sonar:sonar -Dsonar.scm-activity.enabled=true
