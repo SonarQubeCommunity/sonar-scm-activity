e6fa5ee32f955cef7d8afca1f0c0681ba9f4ca72
David Gageot <david@gageot.net>
2012-07-18 15:29:46 +0200

    SONARPLUGINS-1983 Typos in the description of the SCM URL property

--- src/main/java/org/sonar/plugins/scmactivity/ScmActivityPlugin.java
+++ src/main/java/org/sonar/plugins/scmactivity/ScmActivityPlugin.java
@@ -41,7 +41,7 @@ import java.util.List;
     key = ScmActivityPlugin.URL,
     defaultValue = "",
     name = "SCM URL",
-    description = "SCM URL. The format is described in this page.  <a href='http://maven.apache.org/scm/scm-url-format.html'>this page</a>. Example:" +
+    description = "SCM URL. The format is described <a target=\"_blank\" href=\"http://maven.apache.org/scm/scm-url-format.html\">this page</a>. Example: " +
       "<i>scm:svn:https://svn.codehaus.org/sonar-plugins/trunk/scm-activity</i>. This property is mainly used to determine the SCM engine client to be used.",
     module = true,
     project = true,
